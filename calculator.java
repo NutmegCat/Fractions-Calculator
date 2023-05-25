61d55eba32472d9468956503a9245d91b12e05f0
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-05-25 14:43:30 -0400

    added operation methods

--- calculator.java
+++ calculator.java
@@ -24,9 +24,29 @@ class calculator {
         denominator2 = In.getInt();
 
         if (operator == '+') {
-            
-        } else {
-            
+            addition(numerator1, denominator1, numerator2, denominator2);
+        } else if (operator == '-') {
+            subtraction(numerator1, denominator1, numerator2, denominator2);
+        } else if (operator == '*'){
+            multiplication(numerator1, denominator1, numerator2, denominator2);
+        } else if (operator == '/'){
+            division(numerator1, denominator1, numerator2, denominator2);
         }
+    }
+
+    public static void addition(int numAdd1, int denAdd1, int numAdd2, int denAdd2) {
+        System.out.println(numAdd1 + " " + denAdd1 + " " + numAdd2 + " " + denAdd2);
+    }
+
+    public static void subtraction(int numSub1, int denSub1, int numSub2, int denSub2) {
+        System.out.println(numSub1 + " " + denSub1 + " " + numSub2 + " " + denSub2);
+    }
+
+    public static void multiplication(int numMul1, int denMul1, int numMul2, int denMul2) {
+        System.out.println(numMul1 + " " + denMul1 + " " + numMul2 + " " + denMul2);
+    }
+
+    public static void division(int numDiv1, int denDiv1, int numDiv2, int denDiv2) {
+        System.out.println(numDiv1 + " " + denDiv1 + " " + numDiv2 + " " + denDiv2);
     }
 }
